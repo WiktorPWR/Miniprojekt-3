1bdba04edee3ad4b4714218e050ca102b17c54bd
Piotr <272503@student.pwr.edu.pl>
2024-06-07 12:36:55 +0200

    zmiana klasy Node na strukture wewnątrz klasy, dodanie konstruktorów, dodanie metody rehash, oraz metody maintainloadfactortop i bot, zmiana konstruktora klasy Open addresing, poprawa metody clear

--- Hash_table_OA.cpp
+++ Hash_table_OA.cpp
@@ -5,105 +5,129 @@ using namespace std;
 // Szablon dla typu generycznego
 template <typename K, typename V>
 
-// Klasa Node
-class Node {
-public:
-    V value;
-    K key;
-
-    // Konstruktor Node
-    Node(K key, V value)
-    {
-        this->value = value;
-        this->key = key;
-
-    }
-};
-
-// Szablon dla typu generycznego
-template <typename K, typename V>
-
 // Nasza własna klasa Map
 class Open_address {
-    // Tablica elementów haszowanych
-    DynamicArray<Node<K, V>*> table;
-    int capacity;
-    // Aktualny rozmiar
-    int currentSize;
-    // Węzeł dummy
-    Node<K, V>* dummyNode;
-
 public:
-    Open_address()
-    {
-        // Początkowa pojemność tablicy haszowanej
-        capacity = 20;
-        currentSize = 0;
-        for (int i = 0; i < capacity; i++)
-            table.push_back(nullptr);
+    class Node {
+    public:
+        V value;
+        K key;
+        Node() : key(K()), value(V()) {}
+        // Konstruktor Node
+        Node(K key, V value) {
+            this->value = value;
+            this->key = key;
+        }
+    };
 
-        dummyNode = new Node<K, V>(-1, -1);
+private:
+    int capacity;      // Size of the hash table
+    int currentSize;        // Number of elements in the hash table
+    Node* table;        // Hash table
+    Node dummyNode;
+
+    int hash(K key) const {
+        return std::abs(key) % capacity;
     }
 
-    // Funkcja implementująca funkcję haszującą, aby znaleźć indeks dla klucza
-    int computeHash(K key) const { return key % capacity; }
+    void rehash() {
+        int old_size = capacity;
+        capacity *= 2;
+
+        Node* new_array = new Node[capacity]();
+        Node* old_array = table;
+
+        table = new_array;
+        currentSize = 0;
+
+        for (int i = 0; i < old_size; ++i) {
+            if (old_array[i].key != K()) {
+                addElement(old_array[i].key, old_array[i].value);
+            }
+        }
+
+        delete[] old_array;
+    }
+
+    void maintainLoadFactorTop() {
+        float load_factor = static_cast<float>(currentSize) / static_cast<float>(capacity);
+        if (load_factor > 0.75f) {
+            rehash();
+        }
+    }
+
+    void maintainLoadFactorBot() {
+        float load_factor = static_cast<float>(currentSize) / static_cast<float>(capacity);
+        if (load_factor < 0.25f) {
+            rehash();
+        }
+    }
+
+public:
+    Open_address(int size = 100) : capacity(size), currentSize(0) {
+        table = new Node[capacity]();
+        dummyNode = Node(K(), V());
+    }
 
     // Funkcja do dodawania pary klucz-wartość
-    void addElement(K key, V value)
-    {
-        Node<K, V>* newNode = new Node<K, V>(key, value);
+    void addElement(K key, V value) {
+
+        Node newNode(key, value);
 
         // Zastosuj funkcję haszującą, aby znaleźć indeks dla podanego klucza
-        int index = computeHash(key);
+        int index = hash(key);
 
         // Znajdź następne wolne miejsce
-        while (table[index] != nullptr
-               && table[index]->key != key
-               && table[index]->key != -1) {
+        while (table[index].key != K()
+               && table[index].key != key
+               && table[index].key != dummyNode.key) {
             index++;
             index %= capacity;
         }
 
         // Jeśli nowy węzeł ma być wstawiony, zwiększ aktualny rozmiar
-        if (table[index] == nullptr || table[index]->key == -1)
+        if (table[index].key == K() || table[index].key == dummyNode.key) {
             currentSize++;
+            maintainLoadFactorTop();
+        }
+
         table[index] = newNode;
     }
 
     // Funkcja do usuwania pary klucz-wartość
-    V removeElement(int key)
-    {
+    V removeElement(K key) {
         // Zastosuj funkcję haszującą, aby znaleźć indeks dla podanego klucza
-        int index = computeHash(key);
+        int index = hash(key);
 
         // Znajdź węzeł z podanym kluczem
-        while (table[index] != nullptr) {
+        while (table[index].key != K()) {
             // Jeśli znaleziono węzeł
-            if (table[index]->key == key) {
-                Node<K, V>* temp = table[index];
+            if (table[index].key == key) {
+                Node temp = table[index];
 
                 // Wstaw tutaj węzeł dummy do dalszego użycia
                 table[index] = dummyNode;
 
                 // Zmniejsz rozmiar
                 currentSize--;
-                return temp->value;
+                maintainLoadFactorBot();
+                return temp.value;
             }
             index++;
             index %= capacity;
         }
 
-        // Jeśli nie znaleziono, zwróć NULL
-        return V();
+        // Jeśli nie znaleziono, zwróć domyślną wartość typu V
+        //throw std::runtime_error("Element not found");
     }
 
     void clear() {
-        delete table;
+        delete[] table;
+        table = new Node[capacity]();
+        currentSize = 0;
     }
 
-    ~Open_address(){
-        clear();
+    ~Open_address() {
+        delete[] table;
     }
-
-
 };
